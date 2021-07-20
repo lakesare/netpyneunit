c387b2dbb178ea5addfbebaae97abac15d52b29c
Evgenia Karunus <lakesare@gmail.com>
2021-07-20 14:43:15 +0500

    Basic setup

---
+++ netpyneunit/__init__.py
@@ -0,0 +1,6 @@
+"""NetpyneUnit.
+
+A SciUnit package for NetPyNE models.
+"""
+
+from . import tests, models, capabilities, scores
---
+++ setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import os
+import sys
+
+long_description = open("README.md").read()
+with open('requirements.txt', 'r') as fp:
+    install_requires = fp.read()
+
+setup(
+    name="netpyneunit",
+    version='0.1.2',
+    packages=['netpyneunit'],
+    package_data={'netpyneunit':[
+        os.path.join('tests','*.py'),
+        os.path.join('models','*.py'),
+        os.path.join('models/backends','*.py'),
+        os.path.join('capabilities','*.py'),
+        os.path.join('scores','*.py')],
+    },
+    install_requires=install_requires,
+    extras_require={},
+    author="Rick Gerkin & Evgenia Karunus",
+    author_email="rgerkin@asu.edu, lakesare@gmail.com",
+    description="A SciUnit library for testing of NetPyNE models.",
+    long_description=long_description,
+    license="MIT",
+    url='https://github.com/rgerkin/netpyneunit',
+    classifiers=[]
+)
