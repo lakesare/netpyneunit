f29328a11e59e9f4b19455a365c16a3b068a3805
Evgenia Karunus <lakesare@gmail.com>
2021-08-18 17:32:57 +0500

    Added some comments

--- netpyneunit/models/backends/netpyne_backend.py
+++ netpyneunit/models/backends/netpyne_backend.py
@@ -24,46 +24,56 @@ class NetpyneBackend(Backend):
         'pops': sim.net.allPops,
       },
       'simConfig': sim.cfg.__dict__,
-
-      # Results
       'simData': sim.allSimData
     }
 
     if 'LFP' in cache['simData']['allSimData']:
       cache['simData']['allSimData']['LFP'] = cache['simData']['allSimData']['LFP'].tolist()
 
-    # This line changes the first result (remember the colab bug)
-    # cache = sim.replaceDictODict(cache)
+    cache = sim.replaceDictODict(cache)
 
     return cache
 
-  # A final get_sim_hash()!
   def get_sim_hash(self):
+    """
+      Used to determine whether the sim we want to run is already cached.
+
+      Does NOT react to sim.net.modifyConns, modifyStims, and other similar methods.
+      If we want to differentiate between the sims on which `sim.net.modifyConns` was or wasn't called, we want to cache:
+        net['cells'] = sim.net.allCells
+        net['pops'] = sim.net.allPops
+      additionally.
+      However, these can be huge objects, and we might want to consider other options:
+      1. Do we even need to account for `sim.net.modifyConns`? Is this frequently used in papers?
+      2. Instead of looking at sim.net.allCells, every time we change something, we might change the attribute of the NetpyneModel.
+    """
     net = {}
-    # net['params'] = sim.replaceFuncObj(sim.net.params.__dict__)
+    net['params'] = sim.replaceFuncObj(sim.net.params.__dict__)
     net['simConfig'] = sim.cfg.__dict__
-    # print(sim.cfg.__dict__)
-    # net['netpyne_version'] = sim.version(show=False)
+    net['netpyne_version'] = sim.version(show=False)
     return net
 
   def backend_run(self):
     self.model.initSim()
     sim_hash = self.get_sim_hash()
-    # I think sciunit is doing something bad to run_params.
-    # And then NEURON complains.
     self.model.set_run_params(sim_hash=sim_hash)
-
     # Checks whether we have cache, and runs it (with _backend_run) if we don't.
     super().backend_run()
 
   def _backend_run(self):
     # Identical to sim.create():
     #
-    # 1. We leave these up to the user:
+    # 1. We leave these up to the user (they implement it in initSim()):
+    #    ATTENTION: it's prettier to only leave sim.initialize() to the user.
+    #    The reason I also left .createPops() and .createCells() to the user
+    #    is because the PdcmModel randomizes the input AFTER it creates pops & cells.
+    #    When we implement more models it will be more clear what we want
+    #    the user to have in initSim(), and what we want to run for them.
+    #
     #    sim.initialize(netParams, simConfig)
     #    sim.net.createPops()
     #    sim.net.createCells()
-
+    #
     # 2. And we run these ourselves.
     #    The user can override _backend_run if they want to!
     sim.net.connectCells()
